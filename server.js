ffd4ccf743b302c6617f2c452c7b134df678b339
Ali Nguyen <alin57717@gmail.com>
2020-10-09 22:57:39 -0700

    create connection using sql

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
 const db = require(".");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 8889,
+
+    user: "root",
+
+    password: "root",
+    database: "ee_info_db"
+});
