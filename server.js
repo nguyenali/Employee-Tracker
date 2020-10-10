376b9007b6140de05d1c032102635068898e448f
Ali Nguyen <alin57717@gmail.com>
2020-10-09 23:00:00 -0700

    add in error function for connection and to start screen

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
 const db = require(".");
+const { of } = require("rxjs");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,4 +13,11 @@ const connection = mysql.createConnection({
 
     password: "root",
     database: "ee_info_db"
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("connected as id" + connection.threadId);
+
+    startScreen();
 });
