adbc265c7d33b5cc81c14567ae89169110813266
Ali Nguyen <alin57717@gmail.com>
2020-10-10 22:15:11 -0700

    create seperate table for employee in schema

--- db/schema.sql
+++ db/schema.sql
@@ -32,16 +32,18 @@ FOREIGN KEY (department_id) REFERENCES department(id)
 
 CREATE TABLE employee (
 
-id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
-first_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30),
 
-last_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30),
 
-role_id INT NOT NULL,
+  manager_id INT,
 
-manager_id INT,
+  role_id INT,
 
-PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 
 );
