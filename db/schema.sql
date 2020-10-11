e91395c7cf81cd38b6fe477f9664b9501de20670
Ali Nguyen <alin57717@gmail.com>
2020-10-10 22:21:39 -0700

    add in selecting which sql table

--- db/schema.sql
+++ db/schema.sql
@@ -93,3 +93,10 @@ INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Jason", "Baker", 1, 6);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Tom", "Nice", 2, 7);
+
+
+-- SELECTING FOR CREATING 
+--TABLES IN OUR SQL WORKBENCH 
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
