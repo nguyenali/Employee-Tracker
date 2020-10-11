9183ffae8e24228e4309ac92f6b327935270ac40
Ali Nguyen <alin57717@gmail.com>
2020-10-10 21:20:47 -0700

    insert role into value table

---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO role (title, salary, department_id)
+VALUE ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4), ("recruiter", 57500, 1), ("sales person", 85650, 5);
