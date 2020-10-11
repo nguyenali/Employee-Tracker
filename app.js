eaeed292317c5cc415f21484e09c761b1a429cc6
Ali Nguyen <alin57717@gmail.com>
2020-10-10 22:04:12 -0700

    create add role functiofor employee

--- app.js
+++ app.js
@@ -216,7 +216,38 @@ function updateEmployee() {
 
 }
 
+//============= Add Employee Role ==========================//
+function addRole() { 
+  connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+    inquirer.prompt([
+        {
+          name: "Title",
+          type: "input",
+          message: "What is the roles Title?"
+        },
+        {
+          name: "Salary",
+          type: "input",
+          message: "What is the Salary?"
 
+        } 
+    ]).then(function(res) {
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: res.Title,
+              salary: res.Salary,
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                startPrompt();
+            }
+        )
+
+    });
+  });
+  }
 
 
 
