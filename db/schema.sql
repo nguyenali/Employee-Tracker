d34cf04980dc0e03ecb1f0b404330f227ee3f638
Ali Nguyen <alin57717@gmail.com>
2020-10-10 22:19:38 -0700

    create seeds to add role values inside of schema

--- db/schema.sql
+++ db/schema.sql
@@ -58,3 +58,20 @@ INSERT INTO department (name)
 VALUE ("Finance");
 INSERT INTO department (name)
 VALUE ("Legal");
+
+
+-- EMPLOYEE ROLE SEEDS -------
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
